364db6a411832b18fd7200da4ba1236862f46e83
noah0316 <noa316@naver.com>
2020-08-15 23:35:46 +0900

    Project infra Structure

---
+++ src/Routes/Main/MainContainer.js
@@ -0,0 +1,4 @@
+import React from "react";
+import MainPresenter from "./MainPresenter";
+
+export default class extends React.Component {}
---
+++ src/Routes/Signup/SignupContainer.js
@@ -0,0 +1,4 @@
+import React from "react";
+import SignupPresenter from "./SignupPresenter";
+
+export default class extends React.Component {}
