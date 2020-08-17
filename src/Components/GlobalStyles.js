fc5c691d5e22e783c18310dbdf6b8009940124dc
13circle <13circle97@gmail.com>
2020-08-18 02:30:58 +0900

    Apply GlobalStyles for #root

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -7,6 +7,12 @@ const globalStyles = createGlobalStyle`
         font-family: 'GmarketSansBold';
         src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansBold.woff') format('woff');
     }
+    html, body, #root {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 100%;
+    }
     body {
         font-family: 'GmarketSansBold';
         font-weight: normal;
