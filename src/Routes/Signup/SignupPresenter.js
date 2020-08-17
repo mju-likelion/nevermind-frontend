9d86ac2efcd9aeb60f6a21aa466ce0f1ea1f7eb1
graceFor <tbgusrud12@gmail.com>
2020-08-18 02:26:09 +0900

    signup page

--- src/Routes/Signup/SignupPresenter.js
+++ src/Routes/Signup/SignupPresenter.js
@@ -54,8 +54,37 @@ const MainPresenter = () => (
       <InputContainer>
         <form class="was-validated">
           <div class="mb-3">
-            <label for="validationTextarea">Textarea</label>
-            <div class="invalid-feedback">Please enter a message in the textarea.</div>
+            <label for="validationServer01">Name</label>
+            <input
+              type="text"
+              name="username"
+              class="form-control is-invalid"
+              aria-describedby="validatedInputGroupPrepend"
+              required
+            />
+          </div>
+
+          <div class="mb-3">
+            <label for="validationServer01">Cell_Phone</label>
+            <input
+              type="text"
+              name="cellphone"
+              class="form-control is-invalid"
+              aria-describedby="validatedInputGroupPrepend"
+              required
+            />
+          </div>
+
+          <div class="mb-3">
+            <label for="validationServer01">E-mail</label>
+            <input
+              type="text"
+              name="email"
+              name="cellphone"
+              class="form-control is-invalid"
+              aria-describedby="validatedInputGroupPrepend"
+              required
+            />
           </div>
 
           <div class="custom-control custom-checkbox mb-3">
@@ -70,104 +99,6 @@ const MainPresenter = () => (
             </label>
             <div class="invalid-feedback">Example invalid feedback text</div>
           </div>
-
-          <div class="custom-control custom-radio">
-            <input
-              type="radio"
-              class="custom-control-input"
-              id="customControlValidation2"
-              name="radio-stacked"
-              required
-            />
-            <label class="custom-control-label" for="customControlValidation2">
-              Toggle this custom radio
-            </label>
-          </div>
-          <div class="custom-control custom-radio mb-3">
-            <input
-              type="radio"
-              class="custom-control-input"
-              id="customControlValidation3"
-              name="radio-stacked"
-              required
-            />
-            <label class="custom-control-label" for="customControlValidation3">
-              Or toggle this other custom radio
-            </label>
-            <div class="invalid-feedback">More example invalid feedback text</div>
-          </div>
-
-          <div class="mb-3">
-            <select class="custom-select" required>
-              <option value="">Choose...</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
-            <div class="invalid-feedback">Example invalid custom select feedback</div>
-          </div>
-
-          <div class="custom-file mb-3">
-            <input type="file" class="custom-file-input" id="validatedCustomFile" required />
-            <label class="custom-file-label" for="validatedCustomFile">
-              Choose file...
-            </label>
-            <div class="invalid-feedback">Example invalid custom file feedback</div>
-          </div>
-
-          <div class="mb-3">
-            <div class="input-group is-invalid">
-              <div class="input-group-prepend">
-                <span class="input-group-text" id="validatedInputGroupPrepend">
-                  @
-                </span>
-              </div>
-              <input
-                type="text"
-                class="form-control is-invalid"
-                aria-describedby="validatedInputGroupPrepend"
-                required
-              />
-            </div>
-            <div class="invalid-feedback">Example invalid input group feedback</div>
-          </div>
-
-          <div class="mb-3">
-            <div class="input-group is-invalid">
-              <div class="input-group-prepend">
-                <label class="input-group-text" for="validatedInputGroupSelect">
-                  Options
-                </label>
-              </div>
-              <select class="custom-select" id="validatedInputGroupSelect" required>
-                <option value="">Choose...</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
-            </div>
-            <div class="invalid-feedback">Example invalid input group feedback</div>
-          </div>
-
-          <div class="input-group is-invalid">
-            <div class="custom-file">
-              <input
-                type="file"
-                class="custom-file-input"
-                id="validatedInputGroupCustomFile"
-                required
-              />
-              <label class="custom-file-label" for="validatedInputGroupCustomFile">
-                Choose file...
-              </label>
-            </div>
-            <div class="input-group-append">
-              <button class="btn btn-outline-secondary" type="button">
-                Button
-              </button>
-            </div>
-          </div>
-          <div class="invalid-feedback">Example invalid input group feedback</div>
         </form>
       </InputContainer>
     </Form>
