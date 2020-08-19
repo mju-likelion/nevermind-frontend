d034493aa4c52bac6a24f27f7075d17666f5452a
13circle <13circle97@gmail.com>
2020-08-19 22:59:35 +0900

    Updates

---
+++ precache-manifest.c2d5c1af74f25c00095ce2977a2f9597.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02d5e0492027d7c69a5be050da82e58c",
+    "url": "/nevermind-frontend/index.html"
+  },
+  {
+    "revision": "d0d66ace72582f5f31fa",
+    "url": "/nevermind-frontend/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "d0d66ace72582f5f31fa",
+    "url": "/nevermind-frontend/static/js/2.3c7a6b72.chunk.js"
+  },
+  {
+    "revision": "d3031346469a8a2ef527f1ad705ea3b7",
+    "url": "/nevermind-frontend/static/js/2.3c7a6b72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30c37ba3fb2d0194d214",
+    "url": "/nevermind-frontend/static/js/main.ca52a3ce.chunk.js"
+  },
+  {
+    "revision": "1f8339b45f59fccecb6d",
+    "url": "/nevermind-frontend/static/js/runtime-main.2ae3d0f4.js"
+  }
+]);
