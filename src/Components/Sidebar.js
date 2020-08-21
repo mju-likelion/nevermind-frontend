667121ba28bd111a4edd2bc994cb8fb778225482
noah0316 <noa316@naver.com>
2020-08-21 17:47:49 +0900

    building sidebar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,13 +2,52 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-const Container = styled.ul`
+const Container = styled.div`
+  display: grid;
+`;
+
+const NavContainer = styled.ul`
   margin-top: 5%;
   margin-left: 6%;
   width: 17%;
   background-color: white;
 `;
 
-const Sidebar = () => <Container></Container>;
+const Item = styled.li`
+  text-align: center;
+  :visited {
+    background-color: black;
+    color: red;
+  }
+`;
+
+const Button = styled.button`
+  all: unset;
+  display: flex;
+  margin-top: 10%;
+
+  :hover {
+    background-color: #ebebeb;
+  }
+`;
+const Sidebar = () => (
+  <NavContainer>
+    <Button>
+      <Item>ALL</Item>
+    </Button>
+    <Button>
+      <Item>WEEK</Item>
+    </Button>
+    <Button>
+      <Item>MONTH</Item>
+    </Button>
+    <Button>
+      <Item>YEAR</Item>
+    </Button>
+    <Button>
+      <Item>LIFE TIME</Item>
+    </Button>
+  </NavContainer>
+);
 
 export default Sidebar;
