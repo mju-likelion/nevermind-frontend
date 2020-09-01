dba4f44278fe64c9aa141b9a654da0e015d58895
13circle <13circle97@gmail.com>
2020-09-02 01:20:55 +0900

    Code Decorations - index.html: Loading phrase in title - config-overrides.js: 'Src' path alias

--- config-overrides.js
+++ config-overrides.js
@@ -5,6 +5,7 @@ const path = require("path");
 module.exports = function override(config, env) {
   config.resolve.alias = {
     ...config.resolve.alias,
+    Src: path.resolve(__dirname, "src/"),
     Components: path.resolve(__dirname, "src/Components/"),
     Routes: path.resolve(__dirname, "src/Routes/"),
     assets: path.resolve(__dirname, "src/assets/"),
