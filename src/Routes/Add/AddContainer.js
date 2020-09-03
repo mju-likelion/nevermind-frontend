83fef1462a1f38a80b1860b6380c73df97a75157
graceFor <tbgusrud12@gmail.com>
2020-09-03 17:20:55 +0900

    Add

---
+++ src/Routes/Add/AddContainer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import LoginPresenter from "./AddPresenter";
+import nevAxios from "Src/nev-axios";
+
+export default class extends React.Component {
+  render() {
+    return <AddPresenter />;
+  }
+}
---
+++ src/Routes/Add/AddPresenter.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import Helmet from "react-helmet";
+import Logo from "Components/Logo";
+import { Link } from "react-router-dom";
+
+export default AddPresenter;
