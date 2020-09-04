93bc62d4bffd991f206f2a81f62a9a4e64d41940
graceFor <tbgusrud12@gmail.com>
2020-09-04 13:57:30 +0900

    [AddContainer and AddPresenter]

--- src/Routes/Add/AddContainer.js
+++ src/Routes/Add/AddContainer.js
@@ -1,9 +1,45 @@
 import React from "react";
+import DayPickerInput from "react-day-picker/DayPickerInput";
+import "react-day-picker/lib/style.css";
 import LoginPresenter from "./AddPresenter";
 import nevAxios from "Src/nev-axios";
 
 export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleDayChange = this.handleDayChange.bind(this);
+    this.state = {
+      appName: null,
+      bill: 0,
+      type: "all",
+      startDay: undefined,
+      endDay: undefined,
+    };
+  }
+
+  handleStartDayChange = (day) => {
+    this.setState({ startDay: day });
+  };
+
+  handleEndDayChange = (day) => {
+    this.setState({ endDay: day });
+  };
+
+  onSubmit = (appName, bill, type, startDay, endDay) => {};
+
   render() {
-    return <AddPresenter />;
+    const { appName, bill, type, startDay, endDay } = this.state;
+    return (
+      <AddPresenter
+        appName={appName}
+        bill={bill}
+        type={type}
+        startDay={startDay}
+        endDay={endDay}
+        onSubmit={this.onSubmit}
+        handleStartDayChange={this.handleStartDayChange}
+        handleEndDayChange={this.handleEndDayChange}
+      />
+    );
   }
 }
--- src/Routes/Add/AddPresenter.js
+++ src/Routes/Add/AddPresenter.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Helmet from "react-helmet";
-import Logo from "Components/Logo";
 import { Link } from "react-router-dom";
+import Calendar from "react-calendar";
 
 export default AddPresenter;
