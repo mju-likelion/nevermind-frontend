b879915aad57d089c7ed70175fa1a471addeaed0
13circle <13circle97@gmail.com>
2020-09-06 14:50:19 +0900

    Conditional Rendering with Session Validation
    * Navbar.js (+ logout.png)
    - Conditional Redering with nevAxios.issession
    - Update component for each routing
      > Use withRouter
    - Temporary logout image & button
      > To use nevAxios.logout
    
    * Router.js
    - Route to Service component only if the session exists
    - Will apply more efficient conditional routing later

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,14 @@
 // 로그인 된 후의 상황인 Navbar
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import notice from "assets/notice.svg";
 import home from "assets/home.svg";
 import service from "assets/service.svg";
 import user from "assets/user.svg";
+import logout from "assets/logout.png"; // 임시 로그아웃 이미지
+import { withRouter } from "react-router";
 import { Link } from "react-router-dom";
+import nevAxios from "Src/nev-axios";
 
 const Container = styled.div`
   margin: 0;
@@ -13,29 +16,63 @@ const Container = styled.div`
   background-color: #263d66;
   width: 5em;
   height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
   position: fixed;
   overflow: auto;
 `;
 
+const ImgLinkContainer = styled.div`
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow: auto;
+`;
+
 const ImgLink = ({ to, imgSrc }) => (
-  <Link to={to} className="d-flex  h-100">
+  <Link to={to} className="d-flex h-100">
     <button type="button" className="btn">
       <img src={imgSrc} />
     </button>
   </Link>
 );
 
-const Navbar = () => (
-  <Container className="py-5">
-    <ImgLink to="Main" imgSrc={home} />
-    <ImgLink to="Main" imgSrc={user} />
-    <ImgLink to="Main" imgSrc={notice} />
-    <ImgLink to="Service" imgSrc={service} />
-  </Container>
-);
+const Navbar = () => {
+  const [isSession, setSessionState] = useState(false);
+  useEffect(() => {
+    (async () => {
+      const sessionRes = await nevAxios.issession();
+      setSessionState(sessionRes.data.is_session);
+    })();
+  });
 
-export default Navbar;
+  // 임시 로그아웃 버튼 삽입
+  return (
+    <Container className="py-5">
+      {isSession ? (
+        <ImgLinkContainer>
+          <ImgLink to="Main" imgSrc={home} />
+          <ImgLink to="Main" imgSrc={user} />
+          <ImgLink to="Main" imgSrc={notice} />
+          <ImgLink to="Service" imgSrc={service} />
+          <Link
+            to="Main"
+            className="d-flex h-100"
+            onClick={(e) => nevAxios.logout()}
+          >
+            <button type="button" className="btn">
+              <img src={logout} />
+            </button>
+          </Link>
+        </ImgLinkContainer>
+      ) : (
+        <div></div>
+      )}
+    </Container>
+  );
+};
+
+export default withRouter(Navbar);
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,30 +1,49 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import { withRouter } from "react-router";
 import styled from "styled-components";
 import Main from "Routes/Main";
 import Signup from "Routes/Signup";
 import Login from "Routes/Login";
 import Service from "Routes/Service";
 import Navbar from "./Navbar";
+import nevAxios from "Src/nev-axios";
 
 const MainContainer = styled.div`
   margin-left: 5em;
   height: 100%;
 `;
 
-const Routers = () => (
-  <Router>
-    <Navbar />
-    <MainContainer>
-      <Switch>
-        <Route path="/" exact component={Main} />
-        <Route path="/Signup" component={Signup} />
-        <Route path="/Login" component={Login} />
-        <Route path="/Service" component={Service} />
-        <Redirect from="*" to="/" />
-      </Switch>
-    </MainContainer>
-  </Router>
-);
+const Routers = () => {
+  const [isSession, setSessionState] = useState(false);
+  useEffect(() => {
+    (async () => {
+      const sessionRes = await nevAxios.issession();
+      setSessionState(sessionRes.data.is_session);
+    })();
+  });
+  return (
+    <Router>
+      <Navbar />
+      <MainContainer>
+        <Switch>
+          <Route path="/" exact component={Main} />
+          <Route path="/Signup" component={Signup} />
+          <Route path="/Login" component={Login} />
+          <Route
+            path={isSession ? "/Service" : "/"}
+            component={isSession ? Service : Main}
+          />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </MainContainer>
+    </Router>
+  );
+};
 
 export default Routers;
