6b3ff5d006a6c4735aed91d0bf2d4c9bf1b119a8
13circle <13circle97@gmail.com>
2020-09-08 01:11:01 +0900

    Fix duplicate modal close

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -1,32 +1,44 @@
 import React from "react";
 import PropTypes from "prop-types";
+import $ from "jquery";
 import styled from "styled-components";
 
 const Poster = () => (
   <div
-    class="modal"
+    className="modal"
     id="appList"
     tabindex="-1"
     role="dialog"
     aria-labelledby="exampleModalLabel"
     aria-hidden="true"
   >
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">
+    <div className="modal-dialog">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h5 className="modal-title" id="exampleModalLabel">
             Modal title
           </h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <button
+            type="button"
+            className="close"
+            data-dismiss="modal"
+            aria-label="Close"
+          >
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        <div class="modal-body">...</div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">
+        <div className="modal-body">...</div>
+        <div className="modal-footer">
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={(e) => {
+              $("#appList").modal("hide");
+            }}
+          >
             Close
           </button>
-          <button type="button" class="btn btn-primary">
+          <button type="button" className="btn btn-primary">
             Save changes
           </button>
         </div>
