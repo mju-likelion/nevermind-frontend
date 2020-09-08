8cba154fe8d5ba5ef038c682826ca2cf424fddca
noah0316 <noa316@naver.com>
2020-09-08 22:35:12 +0900

    Add Test Code - AddContainer.js : Add Test code for get ios application list information(json type) from https://rss.itunes.apple.com/api/v1/kr/ios-apps/top-free/all/100/explicit.json

--- src/Routes/Add/AddContainer.js
+++ src/Routes/Add/AddContainer.js
@@ -1,17 +1,33 @@
 import React from "react";
 import AddPresenter from "./AddPresenter";
 import nevAxios from "Src/nev-axios";
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://rss.itunes.apple.com/api/v1/kr/ios-apps/",
+});
 
 export default class extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       appName: null,
+      list: null,
       fee: 0,
       type: "all",
       startDate: new Date(),
       endDate: new Date(),
     };
+  }
+
+  async componentDidMount() {
+    try {
+      const {
+        results: { results: list },
+      } = await api.get("top-free/all/100/explicit.json");
+      console.log(list);
+      this.setState(list);
+    } catch {}
   }
 
   handleStartDateChange = (date) => {
