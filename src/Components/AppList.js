132c721ec7db220b23aa7cee1affaf692d087f8a
graceFor <tbgusrud12@gmail.com>
2020-09-20 23:55:11 +0900

    App_List

--- src/Components/AppList.js
+++ src/Components/AppList.js
@@ -16,7 +16,7 @@ const Poster = () => (
       <div className="modal-content">
         <div className="modal-header">
           <h5 className="modal-title" id="exampleModalLabel">
-            App_List
+            App List
           </h5>
           <button type="button" className="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
