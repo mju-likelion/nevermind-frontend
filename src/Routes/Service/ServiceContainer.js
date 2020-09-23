86cfd9d5e16f7ae8fa4ae1c209cc04ab3d1572a0
13circle <13circle97@gmail.com>
2020-09-24 00:24:41 +0900

    ServiceContainer states outline
    NOTE: Must considered as application-layer states
    
    {
      type: $SUBSCRIPTION_TYPE, // Default: "ALL"
      expectedFee: {
        week: $WEEKLY_FEE,
        month: $MONTHLY_FEE,
        year: $YEARLY_FEE,
      },
      paidFee: $PAID_FEE,  // References "type"
      subscribedApps: [
        // From Server-Side, NOT by Client-Side
        {
          appName,
          appImgURL,
          type,
          bill,
          startDate,
          endDate,
        }
      ],
    }

--- src/Routes/Service/ServiceContainer.js
+++ src/Routes/Service/ServiceContainer.js
@@ -1,3 +1,30 @@
+/*
+[ ServiceContainer States Outline ]
+
+NOTE: Must considered as application-layer states
+
+{
+  type: $SUBSCRIPTION_TYPE, // Default: "ALL"
+  expectedFee: {
+    week: $WEEKLY_FEE,
+    month: $MONTHLY_FEE,
+    year: $YEARLY_FEE,
+  },
+  paidFee: $PAID_FEE,  // References "type"
+  subscribedApps: [
+    // From Server-Side, NOT by Client-Side 
+    {
+      appName,
+      appImgURL,
+      type,
+      bill,
+      startDate,
+      endDate,
+    }
+  ],
+}
+*/
+
 import React from "react";
 import ServicePresenter from "./ServicePresenter";
 
