0b8e66124361dea4f1e65aa6b2b012cd5a33aa20
13circle <13circle97@gmail.com>
2020-10-02 22:16:37 +0900

    Connect to addsubscription API
    - No setState for ServiceContainer yet

--- src/Routes/Add/AddContainer.js
+++ src/Routes/Add/AddContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import AddPresenter from "./AddPresenter";
+import nevAxios from "Src/nev-axios";
 
 export default class extends React.Component {
   constructor(props, context) {
@@ -36,14 +37,17 @@ export default class extends React.Component {
   handleSubmit = (appImg, appName, fee, type, startDate, endDate) => {
     // TODO: Send data to server using nev-axios
     //       & refresh ServiceContainer
-    console.log({
-      appImg,
-      appName,
-      fee,
-      type,
-      startDate,
-      endDate,
-    });
+    (async () => {
+      const res = await nevAxios.addsubscription({
+        app_name: appName,
+        app_img_url: appImg,
+        sub_type: type,
+        bill: fee,
+        startdate: startDate.getTime(),
+        enddate: endDate.getTime(),
+      });
+      console.log(res.data);
+    })();
   };
 
   initForm = () => {
--- src/Routes/Service/ServiceContainer.js
+++ src/Routes/Service/ServiceContainer.js
@@ -27,7 +27,6 @@ NOTE: Must considered as application-layer states
 
 import React from "react";
 import ServicePresenter from "./ServicePresenter";
-import nevAxios from "../../nev-axios";
 
 const applist = [
   {
@@ -121,17 +120,6 @@ class ServiceContainer extends React.Component {
     super(props, context);
 
     this.state = {};
-    (async () => {
-      const res = await nevAxios.addsubscription({
-        app_name: "Among Us!",
-        app_img_url: "https://is2-ssl.mzstatic.com/image/thumb/Purple114/v4/d5/14/a7/d514a7fb-69e6-7519-e753-2527d12939f1/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/200x200bb.png",
-        sub_type: "week",
-        bill: 12000,
-        startdate: new Date(),
-        enddate: new Date(),
-      });
-      console.log(res.data);
-    })();
   }
 
   render() {
