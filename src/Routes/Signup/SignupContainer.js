82cb6c1d2b7439ba7d608800b7f87a8dd5d53d36
noah0316 <noa316@naver.com>
2020-10-04 02:36:05 +0900

    Fetch Email verification success or not data  - SignupContainer.js : Fetch Email verificaton success or not data(boolean type) and pass data to SignupPresenter.js - SignupPresenter.js : If Email verification is successful, go to the login page and register on the server if Email verification is fails show alert

--- src/Routes/Signup/SignupContainer.js
+++ src/Routes/Signup/SignupContainer.js
@@ -1,6 +1,25 @@
 import React from "react";
+import nevAxios from "Src/nev-axios";
 import SignupPresenter from "./SignupPresenter";
 
-const SignupContainer = () => <SignupPresenter />;
+export default class extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      authnum: false,
+    };
+  }
 
-export default SignupContainer;
+  async componentDidMount() {
+    try {
+      //TODO: Fetch EmailAuth Data Using nev-Axios
+    } catch {
+      console.log("can't find Email verification success code ");
+    }
+  }
+
+  render() {
+    const { authnum } = this.state;
+    return <SignupPresenter authnum={authnum} />;
+  }
+}
